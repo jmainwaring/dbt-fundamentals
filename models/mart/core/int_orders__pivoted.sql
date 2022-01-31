52016d45eab86bc52f22f18ef321300a4fc85f86
jmainwaring@berkeley.edu <jmainwaring@berkeley.edu>
2022-01-31 22:13:00 +0000

    Added first jinja block

---
+++ models/mart/core/int_orders__pivoted.sql
@@ -0,0 +1,17 @@
+{%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
+
+WITH pivoted_payments AS (
+    SELECT
+           order_id
+        {%- for payment_type in payment_methods %}
+          , SUM(CASE WHEN payment_method = '{{ payment_type }}' THEN amount ELSE 0 END) AS {{ payment_type }}_amount
+        {%- endfor %}
+    FROM {{ ref('stg_payments') }}
+    WHERE
+        status = 'success'
+    GROUP BY order_id
+)
+
+SELECT *
+FROM pivoted_payments;
